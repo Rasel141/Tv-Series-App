06f04a2bff28e877d76719c9a9f9f34fddd2d74e
Rasel141 <rrasel141@gmail.com>
2018-09-04 14:56:41 +0600

    Add - search series

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react'
-import SeriesList from '../../components/SeriesList';
+import React, { Component } from "react";
+import SeriesList from "../../components/SeriesList";
 
 class Series extends Component {
-    state = {
-        series: []
-      }
-    
-    componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-        .then(response => response.json())
-        .then(json => this.setState({ series: json }))
-    }
+  state = {
+    series: [],
+    seriesName: '',
+    isFatching: false
+  };
 
-    render() {
-        return(
-            <div>
-                The length of series array - {this.state.series.length}
-                <SeriesList list = { this.state.series }/>
-            </div>
-        )
-    }
+
+  onSeriesInputChange = e => {
+    this.setState({seriesName: e.target.value, isFatching: true})
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+    .then(response => response.json())
+    .then(json => this.setState({ series: json, isFatching: false }));
+  };
+
+  render() {
+    const {series, seriesName, isFatching} = this.state
+
+    return (
+      <div>
+        {/* The length of series array : {this.state.series.length} */}
+        <div>
+          <input 
+            value= {seriesName} 
+            type="text" 
+            onChange={this.onSeriesInputChange} 
+          />
+        </div>
+        {
+            !isFatching && series.length === 0 && seriesName.trim() === ''
+            &&
+            <p>Please enter series name into the input</p>
+        }
+        {
+           !isFatching && series.length === 0 && seriesName.trim() !== ''
+            &&
+            <p>No TV series have been found with this name</p>
+        }
+        {
+            isFatching && <p>Loading...</p>
+        }
+        {
+            !isFatching && <SeriesList list={this.state.series} />
+        }
+
+      </div>
+    );
+  }
 }
 
-export default Series
+export default Series;
